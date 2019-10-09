706a35513b255c61f57bef09a5bd839bdb448eca
ahmadghzawi <ahmd.ghzawi@gmail.com>
2019-10-09 17:29:20 +0300

    adding add component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import Add from './components/Add';
+import Table from './components/Table';
 
 export default class App extends Component {
+
   state = {
+    
     repos: [
       {
         id: 1,
@@ -15,12 +19,24 @@ export default class App extends Component {
         status: 'Public',
         language: 'JavaScript'
       }
-    ]
+    ],
+    
   };
+
+
+  add = event => {
+    event.preventDefault();
+    console.log(event.target['repo-title'].value, event.target['repo-language'].value, event.target['repo-status'].value);
+  }
+
   render() {
     return (
-      <div style={{ border: 'black 1px solid' }}>
+      <div className="container-fluid" style={{ border: 'black 1px solid' }}>
         <h6>App</h6>
+        <div className="container">
+          <Add add={this.add} />
+          <Table />
+        </div>
       </div>
     );
   }
--- src/components/Add.js
+++ src/components/Add.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
 
 export default class Add extends Component {
+
   render() {
     return (
       <div style={{ border: '3px orange solid' }}>
         <h6>Add</h6>
+        <div>
+          <form onSubmit={this.props.add} className="form-row">
+          <div className="form-group col-md-3">
+              <input type="text" className="form-control" id="repo-title" name='repo-title' placeholder="Repo Title" ref="repo-title"/>
+            </div>
+            <div className="form-group col-md-3">
+              <input type="text" className="form-control" id="repo-language" name='repo-language' placeholder="Repo Language" ref="repo-language"/>
+            </div>
+            <div className="form-group col-md-4">
+              <select className="form-control" id="repo-status" name="repo-status" ref="repo-status"> 
+                <option>Repo Status (Private/Public)</option>
+                <option>Private</option>
+                <option>Public</option>
+              </select>
+            </div>
+            <button type="submit" className="form-group col-md-2 btn btn-primary">Add Repo</button>
+          </form>
+        </div>
       </div>
     );
   }
