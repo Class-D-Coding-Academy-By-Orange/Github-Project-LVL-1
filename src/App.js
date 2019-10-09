c557eed95e29ae620d1029e50519b1406d38b731
AbeerJaafreh <abeeraljaafreh@gmail.com>
2019-10-09 15:08:31 +0300

    first push

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Add from "./components/Add";
+import Table from "./components/Table";
 
 export default class App extends Component {
   state = {
     repos: [
       {
         id: 1,
-        title: 'Array',
-        status: 'Private',
-        language: 'HTML'
+        title: "Array",
+        status: "Private",
+        language: "HTML"
       },
       {
         id: 2,
-        title: 'Object',
-        status: 'Public',
-        language: 'JavaScript'
+        title: "Object",
+        status: "Public",
+        language: "JavaScript"
       }
     ]
   };
   render() {
     return (
-      <div style={{ border: 'black 1px solid' }}>
-        <h6>App</h6>
+      <div style={{ border: "black 1px solid" }}>
+        <h3 style={{ textAlign: "center", color: "red" }}>GitHub Repo</h3>
+        <Add />
+        <Table />
       </div>
     );
   }
--- src/components/Add.js
+++ src/components/Add.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Add extends Component {
   render() {
     return (
-      <div style={{ border: '3px orange solid' }}>
-        <h6>Add</h6>
+      <div style={{ border: "3px orange solid" }}>
+        <input type="text" name="" id="task" class="form-control" placeholder="insert r" />
+        <button id="helpId" style={{float:'right'}} class="text-muted">Add </button>
       </div>
     );
   }
